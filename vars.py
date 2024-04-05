4ece4228c72ec84748331c061c58a6f68b0d14bb
SilentTesting <166071768+SilentTesting@users.noreply.github.com>
2024-04-05 19:07:23 +0530

    vars.py

--- vars.py
+++ vars.py
@@ -1,15 +1,15 @@
   import os
 
 class vars(object):
-    API_ID = int(os.environ.get("APP_ID", "23842900"))
-    API_HASH = os.environ.get("API_HASH", "d21e95895cf2a5b83b0167fdd3b6e541")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5916376879:AAGOiLBT-WVOcEMoThxsP83N4t80hs8qNKw")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOJ4BuwZjqRC7_yOHBcgEAynslI1t8tSINzBnzEw2_ldnYdQx1hIzX1LnqSfIOZXmQt_m42v75FUQVPqaxbqMz3hhB2xhyz7kjvOXwD7oz8Cxr-g-d-zwpV8a59m4AhKE2tVzelE3Dro5DDiDhgAVb0vXSMflsSN88Y6zSqFsDCHuPxDaO6_4AdSZWsxjadwUTq3erhte2mjcqAHPLd1-GYFvz7M3-2oJ_5BdgDCwGwFPWftJmq6wd54RvzkxtN4s82DRiKkWEjSOqNpScW3-NUT2L35RL9QhpvFC9z8beqrlXjsAM65GgtRaV0MeHNZy1ibpPxVbDDgsm_B7F2d0lOb-cqs=")
+    API_ID = "15232891"
+    API_HASH = "d2c47dc40d19d57482396b277f93e6c3"
+    BOT_TOKEN = "6852537755:AAEpoGYN2_MuIZeKvt8Nm6pfMfvjxf3o1d8"
+    STRING_SESSION = "1BVtsOGwBuy_kO9YX8VWq9iK8gHJuohvTOthBOTDil0oHyAyfWbR3CCzuT26U30mXOMMPXLiKY9TJqDDkNhCYUl0WLxlf1FhzNTF0X3n0jrcLZZB3mn-1tKRzOXGpcoa6Dm3pHTEB9UUhg2F3-NCH9HPzFrsN0D0GUSIYxSauHEa-5IRXzPaIj1Z58RMe2lgFkvXOh4MW_N4sNBJKiOAkHOES2c-J7lG24kAEpsgg25RmhHe0d_o0_J1rWAYxTlwfphB35Aaexjucdu_kF4gUjvden5tlf_aOnetikET_MPWiLZBu6kaW_MsBRNVNeJ-8AU_4iY1gSnIVrA00-BlZ8MjBrsE0JIc="
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "NixaMusicXBot")
-    SUPPORT = os.environ.get("SUPPORT", "SankiWorldMF")
-    CHANNEL = os.environ.get("CHANNEL", "NixaWorld")
-    START_IMG = os.environ.get("START_IMG", "https://te.legra.ph/file/3f47f71b0412f10fffe4c.jpg")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://te.legra.ph/file/3f47f71b0412f10fffe4c.jpg")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5470233619")) # required
+    BOT_USERNAME = "HitlersMusicbot"
+    SUPPORT = "TEST_GR0UP"
+    CHANNEL = "TESTlNG_CHANNEL"
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/7d142e86af073b1144824.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG","https://telegra.ph/file/7d142e86af073b1144824.jpg")
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "6691104677")) # required
